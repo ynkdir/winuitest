0eb87607b319c0a94498f861594611417ac382bd
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2025-11-18 23:31:56 +0900

    Workaround for MediaPlayerElement.Source.  ResourceNotFound event is not triggered for it.

--- src/winuitest/mediaplayerelementpage.py
+++ src/winuitest/mediaplayerelementpage.py
@@ -7,4 +7,9 @@ from win32more.winui3 import XamlClass
 class MediaPlayerElementPage(XamlClass, Page):
     def __init__(self):
         super().__init__()
-        self.LoadComponentFromFile(Path(__file__).with_suffix(".xaml"))
+        # TODO: ResourceNotFound event is not triggered for MediaPlayerElement.Source
+        # self.LoadComponentFromFile(Path(__file__).with_suffix(".xaml"))
+        xaml_path = Path(__file__).with_suffix(".xaml")
+        mp4_path = xaml_path.with_name("movie1.mp4").as_posix()
+        xaml = xaml_path.read_text().replace("movie1.mp4", f"ms-appx:///{mp4_path}")
+        self.LoadComponentFromString(xaml, xaml_path)
