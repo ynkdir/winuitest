8c411c468a9167bcb1e7f776bca942ae5d76a5fa
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2026-05-31 14:20:15 +0900

    Add AppBarSeparator

---
+++ src/winuitest/appbarseparatorpage.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from win32more.Microsoft.UI.Xaml.Controls import Page
+from win32more.winui3 import XamlClass
+
+
+class AppBarSeparatorPage(XamlClass, Page):
+    def __init__(self):
+        super().__init__()
+        self.LoadComponentFromFile(Path(__file__).with_suffix(".xaml"))
